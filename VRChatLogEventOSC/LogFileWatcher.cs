9c2c14b7c923d58c6d7fe26e211b6b880b314f4c
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-03 23:28:59 +0900

    Start and pause watching log
    ログの監視の開始と停止に関する処理の追加
    一部変数名修正

--- VRChatLogEventOSC/LogFileWatcher.cs
+++ VRChatLogEventOSC/LogFileWatcher.cs
@@ -3,97 +3,103 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Collections.Generic;
 using System.IO;
-using System.Diagnostics;
 using Reactive.Bindings;
 
 namespace VRChatLogEventOSC
 {
     public sealed class LogFileWatcher : IDisposable
     {
-        private static readonly string _logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "..", "LocalLow", "VRChat", "VRChat");
-        private string _latestLogPath = Directory.GetFiles(_logPath, "output_log_*.txt", SearchOption.TopDirectoryOnly).OrderByDescending(file => Directory.GetCreationTime(file)).First();
+        private static readonly string _logDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "..", "LocalLow", "VRChat", "VRChat");
+        private string _logFilePath = "";
         private readonly FileSystemWatcher _watcher = new()
         {
-            Path = _logPath,
+            Path = _logDirectoryPath,
             Filter = "*.txt",
             NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.FileName,
             IncludeSubdirectories = false
         };
 
         private readonly IDisposable _watchDisposable;
-        private readonly IDisposable _creatDisposable;
+        private readonly IDisposable _fileCreationDisposable;
 
-        // private int _currentLine = 0;
         private long _lastLength = 0;
 
         public float Interval { get; set; } = 0.1f;
 
-        private ReactiveProperty<string> _logLine = new(string.Empty);
+        private readonly ReactiveProperty<string> _logLine = new(string.Empty);
         public IObservable<string> LogLineObservable => _logLine.Skip(1);
+        private bool _isWatching = false;
+        public bool IsDetectFileCreation {get; set;} = false;
+
+        private bool _disposed = false;
 
         public void Dispose()
         {
-            _creatDisposable.Dispose();
+            if (_disposed)
+            {
+                return;
+            }
+            _fileCreationDisposable.Dispose();
             _watchDisposable.Dispose();
+            _disposed = true;
         }
 
         private void LoadLatestLogFile()
         {
-            _latestLogPath = Directory.GetFiles(_logPath, "output_log_*.txt", SearchOption.TopDirectoryOnly).OrderByDescending(file => Directory.GetCreationTime(file)).First();
+            _logFilePath = Directory.GetFiles(_logDirectoryPath, "output_log_*.txt", SearchOption.TopDirectoryOnly).OrderByDescending(file => Directory.GetCreationTime(file)).First();
             _lastLength = 0;
+        }
+
+        public void StartWatchingFromCurrent()
+        {
+            using (var fileStream = new FileStream(_logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                    _lastLength = fileStream.Length;
+            }
+
+            _isWatching = true;
+        }
+
+        public void StartWatchingFromTop()
+        {
+            _lastLength = 0;
+            _isWatching = true;
+        }
+
+        public void StartWatchingFromLatest()
+        {
+            _isWatching = true;
+        }
+
+        public void PauseWatching()
+        {
+            _isWatching = false;
         }
 
         public LogFileWatcher()
         {
-            // VRChat終了時にしか発行されない。
-            // _disposable = Observable.FromEvent<FileSystemEventHandler, FileSystemEventArgs>(
-            //     h => (s, e) => h(e),
-            //     h => _watcher.Changed += h,
-            //     h => _watcher.Changed -= h
-            // )
-            // .Throttle(TimeSpan.FromSeconds(Interval))
-            // .Subscribe(
-            //     e =>
-            //     {
-            //         int lineCount = 0;
-            //         foreach (string line in File.ReadLines(e.FullPath).Skip(_currentLine).Where(line => !string.IsNullOrWhiteSpace(line)))
-            //         {
-            //             lineCount++;
-            //             Debug.Print(line);
-            //         }
-            //         _currentLine = lineCount;
-            //     }
-            // );
-            _creatDisposable = Observable.FromEvent<FileSystemEventHandler, FileSystemEventArgs>(
+            LoadLatestLogFile();
+            _fileCreationDisposable = Observable.FromEvent<FileSystemEventHandler, FileSystemEventArgs>(
                 h => (s, e) => h(e),
                 h => _watcher.Created += h,
                 h => _watcher.Created -= h
-            ).Select(e => e.FullPath).Where(path => path.Contains("output_log_")).Subscribe(path =>
+            ).Where(_ => IsDetectFileCreation).Select(e => e.FullPath).Where(path => path.Contains("output_log_")).Subscribe(path =>
             {
-                _latestLogPath = path;
-                // _currentLine = 0;
+                _logFilePath = path;
                 _lastLength = 0;
-                Debug.Print("OnCreat is called.");
             });
+            _watcher.EnableRaisingEvents = true;
 
             
-            _watchDisposable = Observable.Interval(TimeSpan.FromSeconds(Interval)).Subscribe(_ =>
+            _watchDisposable = Observable.Interval(TimeSpan.FromSeconds(Interval)).Where(_ => _isWatching).Subscribe(_ =>
             {
-                // ファイルがロックされていてIOErrorになる
-                // int lineCount = 0;
-                // foreach (string line in File.ReadLines(_latestLogPath).Skip(_currentLine).Where(line => !string.IsNullOrEmpty(line)))
-                // {
-                //     lineCount++;
-                //     Debug.Print(line);
-                // }
-                // _currentLine += lineCount;
-                if (!File.Exists(_latestLogPath))
+                if (!File.Exists(_logFilePath))
                 {
                     return;
                 }
                 try
                 {
-                    using (var fileStream = new FileStream(_latestLogPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (var fileStream = new FileStream(_logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                     {
                         fileStream.Seek(_lastLength, SeekOrigin.Begin);
                         using (var reader = new StreamReader(fileStream))
@@ -105,7 +111,6 @@ namespace VRChatLogEventOSC
                                 {
                                     continue;
                                 }
-                                // Debug.Print(line);
                                 _logLine.Value = line;
                             }
                             _lastLength = fileStream.Length;
@@ -118,7 +123,6 @@ namespace VRChatLogEventOSC
                     LoadLatestLogFile();
                 }
             });
-            _watcher.EnableRaisingEvents = true;
         }
     }
 
