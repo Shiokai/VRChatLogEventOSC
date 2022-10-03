98f99c33222f0cd9557debbb5141acba6554a55f
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-03 23:30:37 +0900

    Converter for event to osc wip
    イベントの情報から設定と照らしてOSCの値とパスに変換する処理の試験

---
+++ VRChatLogEventOSC/EventToOSCConverter.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using static VRChatLogEventOSC.RegexPattern;
+
+using System.Diagnostics;
+
+namespace VRChatLogEventOSC
+{
+    internal sealed class EventToOSCConverter : IDisposable
+    {
+        private readonly LineClassifier _lineClassifier;
+        private readonly OSCSender _oSCSender;
+        private readonly CompositeDisposable _eventsDisposables = new();
+        public WholeSetting CurrentSetting {get; set;} = new();
+
+        private bool _disposed = false;
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            _lineClassifier.Dispose();
+            _eventsDisposables.Dispose();
+            _oSCSender.Dispose();
+        }
+
+        private bool IsEventMatchSetting(Match match, SingleSetting setting, IEnumerable<string> captures)
+        {
+            bool matchAll = true;
+            foreach (var capture in captures)
+            {
+                string settingCapture = setting.CaptureProperty(capture);
+                string matchCapture = match.Groups[capture].Value;
+
+                if (capture == "ReqInv")
+                {
+                    if (string.IsNullOrEmpty(settingCapture))
+                    {
+                        continue;
+                    }
+                    else if (settingCapture == "None" && string.IsNullOrEmpty(matchCapture))
+                    {
+                        continue;
+                    }
+                    else if (settingCapture == "~canRequestInvite" && "~canRequestInvite" == matchCapture)
+                    {
+                        continue;
+                    }
+                }
+
+                if (string.IsNullOrWhiteSpace(settingCapture))
+                {
+                    continue;
+                }
+
+                if (settingCapture == matchCapture)
+                {
+                    continue;
+                }
+
+                matchAll = false;
+
+
+                if (matchAll == false)
+                {
+                    return false;
+                }
+            }
+
+            return matchAll;
+        }
+        public EventToOSCConverter(LineClassifier lineClassifier, OSCSender oSCSender)
+        {
+            _lineClassifier = lineClassifier;
+            _oSCSender = oSCSender;
+
+            
+            foreach (var type in Enum.GetValues<EventTypeEnum>())
+            {
+                if (type == EventTypeEnum.None || !Regexes.ContainsKey(type))
+                {
+                    continue;
+                }
+
+                var diposable = _lineClassifier.EventReactiveProperties[type]
+                .Where(e => e != null)
+                .Subscribe( e =>
+                {
+                    Match match = Regexes[type].Match(e);
+                    IEnumerable<string> captures = CaptureNames(type);
+                    var tasks = new List<Task>();
+
+                    foreach (var setting in CurrentSetting.Settings[type])
+                    {
+                        // foreach (var capture in CaptureNames(type))
+                        // {
+                        //     if (string.IsNullOrWhiteSpace(setting.CaptureProperty(capture)) || setting.CaptureProperty(capture) == match.Groups[capture].Value)
+                        //     {
+                        //         // _oSCSender.SendMessage(setting.OSCAddress, match.Groups[capture].Value);
+                        //         // _oSCSender.SendMessage(setting.OSCAddress, setting.OSCValue);
+                        //         // _oSCSender.SendMessage(setting.OSCAddress, type.ToString(), match.Groups[capture].Value);
+                        //         Debug.Print(match.Groups[capture].Value);
+                        //     }
+
+                        //     if (CaptureNames(type).Where(capture => capture != "ReqInv").All(capture => string.IsNullOrWhiteSpace(setting.CaptureProperty(capture)) || setting.CaptureProperty(capture) == match.Groups[capture].Value))
+                        //     {
+                        //         _oSCSender.SendMessage(setting.OSCAddress, setting.OSCValue);
+                                
+                        //     }
+                        // }
+                        if (IsEventMatchSetting(match, setting, captures))
+                        {
+                            // _oSCSender.SendMessage(setting.OSCAddress, setting.OSCValue);   
+                            _oSCSender.ButtomMessage(setting.OSCAddress, setting.OSCValue);
+                        }
+                    }
+                });
+
+                _eventsDisposables.Add(diposable);
+            }
+        }
+    }
+}
--- VRChatLogEventOSC/MainWindow.xaml.cs
+++ VRChatLogEventOSC/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Diagnostics;
+
 namespace VRChatLogEventOSC
 {
     /// <summary>
@@ -22,11 +24,31 @@ namespace VRChatLogEventOSC
     {
         private LogFileWatcher _watcher;
         private LineClassifier _lineClassifier;
+        private OSCSender _oscSender;
+        private EventToOSCConverter _eventToOSCConverter;
         public MainWindow()
         {
             InitializeComponent();
             _watcher = new LogFileWatcher();
             _lineClassifier = new LineClassifier(_watcher);
+            _oscSender = new OSCSender();
+            _eventToOSCConverter = new EventToOSCConverter(_lineClassifier, _oscSender);
+            var jsonLoader = new SettingLoader();
+            // var test = WholeSetting.CreateEmptyWholeSettingDict(0);
+            var test = WholeSetting.CreateDefaultWholeSettingDict();
+            // test[RegexPattern.EventTypeEnum.OnPlayerJoined].Add(new(settingName: "Test"));
+            test[RegexPattern.EventTypeEnum.JoinedRoom1Detail].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oSCInt: 1));
+            test[RegexPattern.EventTypeEnum.OnPlayerJoined].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oSCInt: 1));
+            jsonLoader.SaveSetting(new WholeSetting(test));
+            // jsonLoader.WriteAsJson(new SettingBase(settingName: "Test", userName: "Shiokai", message: "hoge"));
+            var setting = jsonLoader.LoadSetting();
+            _eventToOSCConverter.CurrentSetting = setting ?? new(WholeSetting.CreateDefaultWholeSettingDict());
+            _watcher.StartWatchingFromTop();
+            // Debug.Print(setting?.ToString());
+
+
+
+            // _lineClassifier.EventReactiveProperties[RegexPattern.EventTypeEnum.OnPlayerJoined].Subscribe(_ => Debug.Print("============================================"));
         }
     }
 }
