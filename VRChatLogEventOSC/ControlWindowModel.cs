4829b5024f09767ad136850e9b11ab344ddae5fc
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-10 23:46:38 +0900

    Add Control Window (old: MainWindow)
    コントロールウィンドウ(旧メインウィンドウ)を追加

---
+++ VRChatLogEventOSC/ControlWindowModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VRChatLogEventOSC
+{
+    internal class ControlWindowModel
+    {
+        private static ControlWindowModel? _instance;
+        public static ControlWindowModel Instance => _instance ??= new ControlWindowModel();
+        private LogEventModel _logEventModel;
+
+        public void PuaseLogWEvent()
+        {
+            _logEventModel.Pause();
+        }
+
+        public void RestartLogEvent()
+        {
+            _logEventModel.Restart();
+        }
+
+        public void RestartLogEventWithScan()
+        {
+            _logEventModel.RestartWithScan();
+        }
+
+        public void Rescan()
+        {
+            _logEventModel.Rescan();
+        }
+
+        public void QuitApplication()
+        {
+            Application.Current.Shutdown();
+        }
+
+        private ControlWindowModel()
+        {
+            _logEventModel = LogEventModel.Instance;
+        }
+    }
+}
