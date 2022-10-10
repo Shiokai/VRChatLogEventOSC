2f53f95ab668f3e6d208223a1a0010fd9363ea0f
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-10 23:44:58 +0900

    Not window related notify icon
    notify iconがウィンドウ無して起動するように

---
+++ VRChatLogEventOSC/Model/NotifyIconModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Reactive.Disposables;
+
+using System.Diagnostics;
+
+namespace VRChatLogEventOSC
+{
+    public sealed class NotifyIconModel : INotifyPropertyChanged, IDisposable
+    {
+        private static NotifyIconModel? _instance;
+        public static NotifyIconModel Instance => _instance ??= new NotifyIconModel();
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private LogEventModel _logEventModel;
+        public ReadOnlyReactivePropertySlim<bool> IsLogEventRunning;
+
+        private CompositeDisposable _compositeDisposable = new();
+
+        private bool _disposed = false;
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            _logEventModel.Dispose();
+            _compositeDisposable.Dispose();
+        }
+
+        public void PuaseLogWEvent()
+        {
+            _logEventModel.Pause();
+        }
+
+        public void RestartLogEvent()
+        {
+            _logEventModel.Restart();
+        }
+
+        public void OpenControlWindow()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private NotifyIconModel()
+        {
+            _logEventModel = LogEventModel.Instance;
+
+            IsLogEventRunning = _logEventModel.IsRunnging;
+            IsLogEventRunning.AddTo(_compositeDisposable);
+
+            Application.Current.Exit += (obj, args) =>
+            {
+                Dispose();
+            };
+        }
+
+
+    }
+}
---
+++ VRChatLogEventOSC/ViewModel/NotifyIconViewModel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Reactive.Disposables;
+
+using System.Diagnostics;
+
+namespace VRChatLogEventOSC
+{
+    public sealed class NotifyIconViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private NotifyIconWrapper _notifyIcon = new();
+        private NotifyIconModel _model;
+        private string _iconTextBase = "VRChatLogEventOSC: ";
+        private string _statusTextBase = "Status: ";
+
+        private string CurrentStatus => _model.IsLogEventRunning.Value ? "Running" : "Paused";
+
+        private ReactivePropertySlim<string> _status = new(string.Empty);
+        public ReadOnlyReactivePropertySlim<string> Status => _status.ToReadOnlyReactivePropertySlim<string>();
+
+        private CompositeDisposable _compositeDisposable = new();
+
+        private bool _disposed = false;
+
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+            _notifyIcon.Dispose();
+            _model.Dispose();
+            _compositeDisposable.Dispose();
+        }
+        private void Notify(string message)
+        {
+            _notifyIcon.RequestNotify(new NotifyIconWrapper.NotifyRequestRecord
+            {
+                Title = "Notify",
+                Text = message,
+                Duration = 1000
+            });
+        }
+
+        private void Notify(string message, string title)
+        {
+            _notifyIcon.RequestNotify(new NotifyIconWrapper.NotifyRequestRecord
+            {
+                Title = title,
+                Text = message,
+                Duration = 1000
+            });
+        }
+
+        public NotifyIconViewModel()
+        {
+            _model = NotifyIconModel.Instance;
+
+            _notifyIcon.OpenSelected?.Subscribe(_ => _model.OpenControlWindow()).AddTo(_compositeDisposable);
+            _notifyIcon.QuitSelected?.Subscribe(_ => Application.Current.Shutdown()).AddTo(_compositeDisposable);
+            _notifyIcon.PauseSelected?.Subscribe(_ => 
+            {
+                if (_model.IsLogEventRunning.Value)
+                {
+                    _model.PuaseLogWEvent();
+                }
+                else
+                {
+                    _model.RestartLogEvent();
+                }
+            }).AddTo(_compositeDisposable);
+
+            _model.IsLogEventRunning.Subscribe(running => 
+            {
+                _notifyIcon.Text = _iconTextBase + CurrentStatus;
+                _status.Value = _statusTextBase + CurrentStatus;
+            });
+
+            _notifyIcon.DoubleClicked?.Subscribe(_ => _model.OpenControlWindow()).AddTo(_compositeDisposable);
+
+            _notifyIcon.Text = _iconTextBase + CurrentStatus;
+            _status.Value = _statusTextBase + CurrentStatus;
+
+            _compositeDisposable.Add(_status);
+            _compositeDisposable.Add(Status);
+
+            Application.Current.Exit += (obj, args) =>
+            {
+                Dispose();
+            };
+        }
+
+
+    }
+}
