b85330b3339a432c327ec5513c93aabf6879f076
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-10 17:22:10 +0900

    Add notify icon
    systray icon追加

--- VRChatLogEventOSC/MainWindow.xaml.cs
+++ VRChatLogEventOSC/MainWindow.xaml.cs
@@ -22,37 +22,37 @@ namespace VRChatLogEventOSC
     /// </summary>
     public partial class MainWindow : Window
     {
-        private LogFileWatcher _watcher;
-        private LineClassifier _lineClassifier;
-        private OSCSender _oscSender;
-        private EventToOSCConverter _eventToOSCConverter;
+        // private LogFileWatcher _watcher;
+        // private LineClassifier _lineClassifier;
+        // private OSCSender _oscSender;
+        // private EventToOSCConverter _eventToOSCConverter;
         public MainWindow()
         {
             InitializeComponent();
-            _watcher = new LogFileWatcher();
-            _lineClassifier = new LineClassifier(_watcher);
-            _oscSender = new OSCSender();
-            _eventToOSCConverter = new EventToOSCConverter(_lineClassifier, _oscSender);
-            var jsonLoader = new SettingLoader();
+            // _watcher = new LogFileWatcher();
+            // _lineClassifier = new LineClassifier(_watcher);
+            // _oscSender = new OSCSender();
+            // _eventToOSCConverter = new EventToOSCConverter(_lineClassifier, _oscSender);
+            // var jsonLoader = new SettingLoader();
             // var test = WholeSetting.CreateEmptyWholeSettingDict(0);
-            var test = WholeSetting.CreateDefaultWholeSettingDict();
+            // var test = WholeSetting.CreateDefaultWholeSettingDict();
             // test[RegexPattern.EventTypeEnum.OnPlayerJoined].Add(new(settingName: "Test"));
             // for (int i = 0; i < 50; i++)
             // {
             //     test[RegexPattern.EventTypeEnum.JoinedRoomURL].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: i, oscType: SingleSetting.OSCTypeEnum.Toggle, oscAddress: "/avatars/parameters/JoinedRoomURL"));
             // }
-            test[RegexPattern.EventTypeEnum.JoinedRoomURL].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: 5, oscType: SingleSetting.OSCTypeEnum.Toggle, oscAddress: "/avatars/parameters/JoinedRoomURL"));
-            test[RegexPattern.EventTypeEnum.OnPlayerJoined].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: true, oscType: SingleSetting.OSCTypeEnum.Button, oscAddress: "/avatars/parameters/OnPlayerJoined"));
-            test[RegexPattern.EventTypeEnum.OnPlayerLeft].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: true, oscType: SingleSetting.OSCTypeEnum.Button, oscAddress: "/avatars/parameters/OnPlayerLeft"));
+            // test[RegexPattern.EventTypeEnum.JoinedRoomURL].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: 5, oscType: SingleSetting.OSCTypeEnum.Toggle, oscAddress: "/avatars/parameters/JoinedRoomURL"));
+            // test[RegexPattern.EventTypeEnum.OnPlayerJoined].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: true, oscType: SingleSetting.OSCTypeEnum.Button, oscAddress: "/avatars/parameters/OnPlayerJoined"));
+            // test[RegexPattern.EventTypeEnum.OnPlayerLeft].Add(new(reqInv: SingleSetting.ReqInvEnum.None, oscValue: true, oscType: SingleSetting.OSCTypeEnum.Button, oscAddress: "/avatars/parameters/OnPlayerLeft"));
             // jsonLoader.SaveSetting(new WholeSetting(test));
             // jsonLoader.WriteAsJson(new SettingBase(settingName: "Test", userName: "Shiokai", message: "hoge"));
-            var setting = jsonLoader.LoadSetting();
-            Debug.Print(setting?.ToString());
-            _eventToOSCConverter.CurrentSetting = setting ?? new(WholeSetting.CreateDefaultWholeSettingDict());
-            _oscSender.ButtomInterval = 5;
+            // var setting = jsonLoader.LoadSetting();
+            // Debug.Print(setting?.ToString());
+            // _eventToOSCConverter.CurrentSetting = setting ?? new(WholeSetting.CreateDefaultWholeSettingDict());
+            // _oscSender.ButtomInterval = 5;
             // _oscSender.ChangeClient(9005);
-            _watcher.IsDetectFileCreation = true;
-            _watcher.StartWatchingFromTop();
+            // _watcher.IsDetectFileCreation = true;
+            // _watcher.StartWatchingFromTop();
             // _watcher.StartWatchingFromCurrent();
             // Debug.Print(setting?.ToString());
             
---
+++ VRChatLogEventOSC/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using Reactive.Bindings;
+
+using System.Diagnostics;
+
+namespace VRChatLogEventOSC
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private LogEventModel _model;
+        private NotifyIconWrapper _notifyIcon = new();
+        private bool _isRunning = true;
+
+        public MainWindowViewModel()
+        {
+            _model = LogEventModel.Instance;
+            LoadedCommand = new ReactiveCommand();
+            LoadedCommand.Subscribe(Loaded);
+            ClosingCommand = new ReactiveCommand<CancelEventArgs?>();
+            ClosingCommand.Subscribe(e => Closing(e));
+            NotifyCommand = new ReactiveCommand();
+            NotifyCommand.Subscribe(() => Notify("Hello world!"));
+            _notifyIcon.OpenSelected?.Subscribe(_ => WindowStateReactive.Value = WindowState.Normal);
+            _notifyIcon.QuitSelected?.Subscribe(_ => Application.Current.Shutdown());
+            _isRunning = _model.IsRunnging.Value;
+            _notifyIcon.PauseSelected?.Subscribe(_ => 
+            {
+                if (_isRunning)
+                {
+                    _model.Pause();
+                }
+                else
+                {
+                    _model.Restart();
+                }
+            });
+
+            PauseCommand.Subscribe(_model.Pause);
+
+            WindowStateReactive.Subscribe(state =>
+            {
+                ShowInTaskbar.Value = true;
+                ShowInTaskbar.Value = state != WindowState.Minimized;
+                Style.Value = state != WindowState.Minimized ? WindowStyle.SingleBorderWindow : WindowStyle.ToolWindow;
+            });
+
+            _notifyIcon.Text = "Running";
+
+            _model.IsRunnging.Subscribe(running =>
+            {
+                if (running)
+                {
+                    _notifyIcon.Text = "VRChatLogEventOSC: Running";
+
+                }
+                else
+                {
+                    _notifyIcon.Text = "VRChatLogEventOSC: Paused";
+                }
+                _isRunning = running;
+            });
+
+            Application.Current.Exit += (obj, args) =>
+            {
+                _notifyIcon.Dispose();
+                _model.Dispose();
+            };
+        }
+
+        public ReactiveCommand LoadedCommand { get; }
+        public ReactiveCommand<CancelEventArgs?> ClosingCommand { get; }
+        public ReactiveCommand NotifyCommand { get; }
+        public ReactiveCommand PauseCommand { get; } = new();
+
+        public ReactiveProperty<WindowState> WindowStateReactive { get; set; } = new();
+
+        public ReactiveProperty<bool> ShowInTaskbar { get; set; } = new();
+        public ReactiveProperty<WindowStyle> Style { get; set; } = new(WindowStyle.ToolWindow);
+
+        private void Notify(string message)
+        {
+            _notifyIcon.RequestNotify(new NotifyIconWrapper.NotifyRequestRecord
+            {
+                Title = "Notify",
+                Text = message,
+                Duration = 1000
+            });
+        }
+
+        private void Notify(string message, string title)
+        {
+            _notifyIcon.RequestNotify(new NotifyIconWrapper.NotifyRequestRecord
+            {
+                Title = title,
+                Text = message,
+                Duration = 1000
+            });
+        }
+
+        private void Loaded()
+        {
+            WindowStateReactive.Value = WindowState.Minimized;
+        }
+
+        private void Closing(CancelEventArgs? e)
+        {
+            if (e == null)
+                return;
+            e.Cancel = true;
+            WindowStateReactive.Value = WindowState.Minimized;
+        }
+    }
+}
