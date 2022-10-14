75b8b9d29889f5576df66f7bf1cd37c252ee5301
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-14 18:55:18 +0900

    Change from Tab to Button
    タブ機能のデザインが嬉しくなかったのでボタンで疑似タブにすることに

---
+++ VRChatLogEventOSC/SettingWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace VRChatLogEventOSC
+{
+    internal class SettingWindowViewModel
+    {
+        public ReactiveCommand JoinedRoomURLButton { get; init; }
+        public ReactiveCommand JoinedRoomNameButton { get; init; }
+        public ReactiveCommand AcceptFriendRequestButton { get; init; }
+        public ReactiveCommand PlayedVideo1Button { get; init; }
+        public ReactiveCommand PlayedVideo2Button { get; init; }
+        public ReactiveCommand AcceptInviteButton { get; init; }
+        public ReactiveCommand AcceptRequestInviteButton { get; init; }
+        public ReactiveCommand OnPlayerJoinedButton { get; init; }
+        public ReactiveCommand OnPlayerLeftButton { get; init; }
+        public ReactiveCommand TookScreenshotButton { get; init; }
+
+        public ReactivePropertySlim<bool> IsJoinedRoomURLButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsJoinedRoomNameButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsAcceptFriendRequestButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsPlayedVideo1ButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsPlayedVideo2ButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsAcceptInviteButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsAcceptRequestInviteButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsOnPlayerJoinedButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsOnPlayerLeftButtonChecked { get; init; }
+        public ReactivePropertySlim<bool> IsTookScreenshotButtonChecked { get; init; }
+
+        private CompositeDisposable _conpositeDisposable = new();
+
+        public SettingWindowViewModel()
+        {
+            JoinedRoomURLButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            JoinedRoomNameButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            AcceptFriendRequestButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            PlayedVideo1Button = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            PlayedVideo2Button = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            AcceptInviteButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            AcceptRequestInviteButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            OnPlayerJoinedButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            OnPlayerLeftButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+            TookScreenshotButton = new ReactiveCommand().WithSubscribe(() => { }).AddTo(_conpositeDisposable);
+
+            IsJoinedRoomURLButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsJoinedRoomNameButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsAcceptFriendRequestButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsPlayedVideo1ButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsPlayedVideo2ButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsAcceptInviteButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsAcceptRequestInviteButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsOnPlayerJoinedButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsOnPlayerLeftButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+            IsTookScreenshotButtonChecked = new ReactivePropertySlim<bool>().AddTo(_conpositeDisposable);
+
+            IsJoinedRoomURLButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsJoinedRoomNameButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsAcceptFriendRequestButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsPlayedVideo1ButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsPlayedVideo2ButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsAcceptInviteButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsAcceptRequestInviteButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsOnPlayerJoinedButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsOnPlayerLeftButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+            IsTookScreenshotButtonChecked.Where(isChecked => isChecked).Subscribe(_ => {});
+        }
+    }
+}
