d6b60e466689c148e36de1dbd5ec49a2453263ec
Shiokai <103746805+Shiokai@users.noreply.github.com>
2022-10-27 00:01:46 +0900

    Binding Change (with Bug)
    バインディングの修正
    ReactivePropertyの更新がされないんだけどなんで?

--- VRChatLogEventOSC/EditorWindowViewModel.cs
+++ VRChatLogEventOSC/EditorWindowViewModel.cs
@@ -13,6 +13,7 @@ using System.Reactive.Linq;
 using System.Diagnostics;
 
 using VRChatLogEventOSC.Common;
+using static VRChatLogEventOSC.Common.SingleSetting;
 
 namespace VRChatLogEventOSC
 {
@@ -29,14 +30,14 @@ namespace VRChatLogEventOSC
             Invite,
         }
         public ReactivePropertySlim<string> SettingName { get; private set; } = new ReactivePropertySlim<string>(string.Empty);
-        [Required(ErrorMessage = "Required")]
+        // [Required(ErrorMessage = "Required")]
         public ReactiveProperty<string> OSCAddress { get; private set; } = new ReactiveProperty<string>(string.Empty);
         public ReactivePropertySlim<bool?> OSCBool { get; private set; } = new ReactivePropertySlim<bool?>((bool?)null);
         public ReactivePropertySlim<int?> OSCInt { get; private set; } = new ReactivePropertySlim<int?>((int?)null);
         public ReactivePropertySlim<float?> OSCFloat { get; private set; } = new ReactivePropertySlim<float?>((float?)null);
         public ReactivePropertySlim<string?> OSCString { get; private set; } = new ReactivePropertySlim<string?>((string?)null);
-        public ReactivePropertySlim<SingleSetting.OSCValueTypeEnum> OSCValueType { get; private set; } = new ReactivePropertySlim<SingleSetting.OSCValueTypeEnum>(SingleSetting.OSCValueTypeEnum.Int);
-        public ReactivePropertySlim<SingleSetting.OSCTypeEnum> OSCType { get; private set; } = new ReactivePropertySlim<SingleSetting.OSCTypeEnum>(SingleSetting.OSCTypeEnum.Button);
+        public ReactivePropertySlim<OSCValueTypeEnum> OSCValueType { get; private set; } = new ReactivePropertySlim<OSCValueTypeEnum>(OSCValueTypeEnum.Int);
+        public ReactivePropertySlim<OSCTypeEnum> OSCType { get; private set; } = new ReactivePropertySlim<OSCTypeEnum>(OSCTypeEnum.Button);
         public ReactivePropertySlim<string> UserName { get; private set; } = new ReactivePropertySlim<string>(string.Empty);
         public ReactivePropertySlim<string> UserID { get; private set; } = new ReactivePropertySlim<string>(string.Empty);
         public ReactivePropertySlim<string> WorldName { get; private set; } = new ReactivePropertySlim<string>(string.Empty);
@@ -66,8 +67,34 @@ namespace VRChatLogEventOSC
 
         public EditorWindowViewModel()
         {
+            OSCAddress.SetValidateAttribute(() => OSCAddress).AddTo(_compositeDisposable);
+            // OSCBool.AddTo(_compositeDisposable);
+            // OSCInt.AddTo(_compositeDisposable);
+            // OSCFloat.AddTo(_compositeDisposable);
+            // OSCString.AddTo(_compositeDisposable);
+            // OSCValueType.AddTo(_compositeDisposable);
+            // OSCType.AddTo(_compositeDisposable);
+            // UserName.AddTo(_compositeDisposable);
+            // UserID.AddTo(_compositeDisposable);
+            // WorldName.AddTo(_compositeDisposable);
+            // WorldURL.AddTo(_compositeDisposable);
+            // WorldID.AddTo(_compositeDisposable);
+            // InstanceID.AddTo(_compositeDisposable);
+            // InstanceType.AddTo(_compositeDisposable);
+            // WorldUserID.AddTo(_compositeDisposable);
+            // Region.AddTo(_compositeDisposable);
+            // Message.AddTo(_compositeDisposable);
+            // URL.AddTo(_compositeDisposable);
+
             EdittingSetting.Value = new SingleSetting();
-            EdittingSetting.Subscribe(setting => Debug.Print(setting.ToString()));
+            EdittingSetting.Subscribe(setting => Debug.Print(setting.ToString())).AddTo(_compositeDisposable);
+            OSCBool.Subscribe(val => Debug.Print("Bool: " + val?.ToString())).AddTo(_compositeDisposable);
+            OSCInt.Subscribe(val => Debug.Print("Int: " + val?.ToString())).AddTo(_compositeDisposable);
+            OSCFloat.Subscribe(val => Debug.Print("Float: " + val?.ToString())).AddTo(_compositeDisposable);
+            OSCString.Subscribe(val => Debug.Print("String: " + val?.ToString())).AddTo(_compositeDisposable);
+            OSCAddress.Subscribe(val => Debug.Print("Address: " + val?.ToString())).AddTo(_compositeDisposable);
+            Message.Subscribe(val => Debug.Print("Message: " + val?.ToString())).AddTo(_compositeDisposable);
+            SettingName.Subscribe(val => Debug.Print("SettingName: " + val?.ToString())).AddTo(_compositeDisposable);
         }
     }
 }
